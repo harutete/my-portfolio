e506fb754c222188c98620e5aa73d3cd0066360e
harutete <ai.takeuchi.10@gmail.com>
2020-06-19 02:10:20 +0900

    Fix: タイトルのスタイルを修正

--- src/components/Home/PageTitlte.tsx
+++ src/components/Home/PageTitlte.tsx
@@ -1,29 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const PageTitle = styled.h1`
-  @import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400;700&display=swap');
+const Wrapper = styled.div`
+@import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400;700&display=swap');
 
-  font-family: 'Caveat', cursive;
+font-family: 'Caveat', cursive;
+text-shadow: 0 2px 4px rgba(0, 0, 0, .25);
+`
+const PageTitle = styled.h1`
   font-size: 10rem;
   font-weight: bold;
 `
 
 const PageSubTitle = styled.p`
-  @import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400;700&display=swap');
-
-  font-family: 'Caveat', cursive;
   font-size: 5.5rem;
   margin-top: 20px;
 `
 
 const PagetitleWrapper = () => {
   return (
-    <div>
+    <Wrapper>
       <PageTitle>AI TAKEUCHI's<br />
       Portforio</PageTitle>
       <PageSubTitle>Frontend Engineer.</PageSubTitle>
-    </div>
+    </Wrapper>
   )
 }
 
