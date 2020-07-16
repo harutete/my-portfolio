26da3afeaa082875ac36d3013c6532de6ef90c2f
harutete <ai.takeuchi.10@gmail.com>
2020-07-16 21:52:57 +0900

    Update: 要素のコンポーネント化

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -8,34 +8,71 @@ import PrimaryHeading from '../common/PrimaryHeading'
 const ProfileContentsWrapper = styled(detailContentsWrapper)`
   background: rgba(201, 159, 255, 0.8);
 `
+const Heading02 = styled.h2`
+  font-size: 1.8rem;
+  font-weight: normal;
+  margin-top: 30px;
+`
+const SkillList = styled.dl`
+  dt {
+    margin-top: 15px;
+    &::before {
+      content: '●';
+      display: inline-block;
+      margin-right: 5px;
+    }
+  }
+  dd {
+    margin-top: 15px;
+  }
+`
+const ToolList = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: -5px;
+  li {
+    marign: 5px 0 0 5px;
+  }
+`
+const SnsLinkList = styled(ToolList)`
+  margin-top: 20px;
+  li {
+    width: 60px;
+    img {
+      max-width: 100%;
+    }
+  }
+`
+
 const Profile = () => {
   return(
     <ProfileContentsWrapper>
       <PrimaryHeading>Profile</PrimaryHeading>
-      <h2>[Skill]</h2>
-      <dl>
+      <Heading02>[Skill]</Heading02>
+      <SkillList>
         {mySkill.contents.map((skill, index) =>
           <>
             <dt key={`skill_${index}`}>
               {skill.name}
             </dt>
             <dd>
-              <ul>
+              <ToolList>
                 {skill.tools.map(tool =>
                   <li>{tool}</li>
                 )}
-              </ul>
+              </ToolList>
             </dd>
           </>
         )}
-      </dl>
-      <ul>
+      </SkillList>
+      <SnsLinkList>
         <li>
           <a href="https://github.com/harutete" target="_blank">
             <img src={'/ico_github.png'} alt="" />
           </a>
         </li>
-      </ul>
+      </SnsLinkList>
     </ProfileContentsWrapper>
   )
 }
