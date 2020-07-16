16e2b1fbb5111b3fdbc366debb14c74ec276e895
harutete <ai.takeuchi.10@gmail.com>
2020-07-16 21:15:54 +0900

    Add: profileのコンテンツを出すところまで

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import mySkill from '../../../content/mySkill.json'
 
 import { detailContentsWrapper } from '../common/ContentsWrapper'
 import PrimaryHeading from '../common/PrimaryHeading'
@@ -7,40 +8,36 @@ import PrimaryHeading from '../common/PrimaryHeading'
 const ProfileContentsWrapper = styled(detailContentsWrapper)`
   background: rgba(201, 159, 255, 0.8);
 `
-const Profile = () => (
-  <ProfileContentsWrapper>
-    <PrimaryHeading>Profile</PrimaryHeading>
-    <h2>[Skill]</h2>
-    <dl>
-      <dt>●Design</dt>
-      <dd>
-        <ul>
-          <li>PhotoShop</li>
-          <li>Illustrator</li>
-        </ul>
-      </dd>
-      <dt>●Markup&Programming</dt>
-      <dd>
-        <ul>
-          <li>HTML</li>
-          <li>CSS(SCSS)</li>
-          <li>JavaScript</li>
-          <li>jQuery</li>
-          <li>TypeScript</li>
-          <li>Vue.js</li>
-          <li>Nuxt.js</li>
-          <li>React</li>
-        </ul>
-      </dd>
-    </dl>
-    <ul>
-      <li>
-        <a href="https://github.com/harutete" target="_blank">
-          <img src={'/ico_github.png'} alt="" />
-        </a>
-      </li>
-    </ul>
-  </ProfileContentsWrapper>
-)
+const Profile = () => {
+  return(
+    <ProfileContentsWrapper>
+      <PrimaryHeading>Profile</PrimaryHeading>
+      <h2>[Skill]</h2>
+      <dl>
+        {mySkill.contents.map((skill, index) =>
+          <>
+            <dt key={`skill_${index}`}>
+              {skill.name}
+            </dt>
+            <dd>
+              <ul>
+                {skill.tools.map(tool =>
+                  <li>{tool}</li>
+                )}
+              </ul>
+            </dd>
+          </>
+        )}
+      </dl>
+      <ul>
+        <li>
+          <a href="https://github.com/harutete" target="_blank">
+            <img src={'/ico_github.png'} alt="" />
+          </a>
+        </li>
+      </ul>
+    </ProfileContentsWrapper>
+  )
+}
 
 export default Profile
