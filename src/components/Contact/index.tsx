81514474dbd044fc8eb690ad2293fa2b7cb57f7b
harutete <ai.takeuchi.10@gmail.com>
2020-07-16 21:53:13 +0900

    Update: Contactページにテキスト追加

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -7,9 +7,20 @@ import PrimaryHeading from '../common/PrimaryHeading'
 const ContactContentsWrapper = styled(detailContentsWrapper)`
   background: rgba(159, 221, 255, 0.8);
 `
+const AddressText = styled.p`
+  font-size: 1.8rem;
+  margin-top: 20px;
+`
+const AnnotationText = styled.p`
+  font-size: 1.2rem;
+  margin-top: 10px;
+`
+
 const Contact = () => (
   <ContactContentsWrapper>
     <PrimaryHeading>Contact</PrimaryHeading>
+    <AddressText>ai.takeuchi.10 [at] gmail.com</AddressText>
+    <AnnotationText><sup>*</sup> at → @</AnnotationText>
   </ContactContentsWrapper>
 )
 
