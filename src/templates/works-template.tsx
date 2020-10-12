739aec7128b71a2a725abb25d83f4ccb19ec05bc
harutete <ai.takeuchi.10@gmail.com>
2020-10-13 01:15:21 +0900

    Update: mdに項目追加

--- src/templates/works-template.tsx
+++ src/templates/works-template.tsx
@@ -1,14 +1,44 @@
 import React from 'react'
 import { graphql } from "gatsby"
+import styled from 'styled-components'
 import Layout from '../components/common/layouts'
+
+const TagsList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  margin-left: -5px;
+  li {
+    border-radius: 2px;
+    border-style: solid;
+    border-width: 1px;
+    margin: 10px 0 0 5px;
+    padding: 2px 5px;
+  }
+`
 
 const WorksTemplate = ({ data }) => {
   const post = data.markdownRemark
+  const tagStyle = (color: string) => ({
+    borderColor: color,
+    color: color
+  })
   return (
     <>
       <Layout />
       <div>
         <h1>{post.frontmatter.title}</h1>
+        <TagsList>
+          {post.frontmatter.tags.map((aaa, index) =>{
+            console.log({aaa})
+            return (
+              <li key={`${index}`}>
+                {aaa.name}
+              </li>
+            )
+          }
+          )}
+        </TagsList>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
     </>
@@ -20,6 +50,10 @@ export const query = graphql`
       html
       frontmatter {
         title
+        tags {
+          name
+          color
+        }
       }
     }
   }
