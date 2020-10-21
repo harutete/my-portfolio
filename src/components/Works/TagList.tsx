72ec1308621588d37f80ccdda8da01257dfd3285
harutete <ai.takeuchi.10@gmail.com>
2020-10-22 00:15:05 +0900

    Add: TagList.tsx

---
+++ src/components/Works/TagList.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+const TagsList = ({ data }) => {
+  const List = styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+    margin-left: -5px;
+    li {
+      border-radius: 2px;
+      border-style: solid;
+      border-width: 1px;
+      margin: 10px 0 0 5px;
+      padding: 2px 5px;
+    }
+  `
+  const tagStyle = (color: string) => ({
+    borderColor: color,
+    color: color
+  })
+  return (
+    <List>
+      {data.map((tag, index) =>
+        <li key={`tag_${index}`} style={tagStyle(tag.color)}>
+          {tag.name}
+        </li>
+      )}
+    </List>
+  )
+}
+
+export default TagsList
