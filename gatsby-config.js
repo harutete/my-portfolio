317df519c1fb83e905ba889a33872a9dec14af98
harutete <ai.takeuchi.10@gmail.com>
2020-10-25 01:48:49 +0900

    Add: gatsby-plugin-transition-link

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-transformer-remark`,
+    `gatsby-plugin-transition-link`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
