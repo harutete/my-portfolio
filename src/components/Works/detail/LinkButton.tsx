d660a6970aa5ebd1cb795203f7be8c3bac76dc9e
harutete <ai.takeuchi.10@gmail.com>
2020-10-26 09:09:51 +0900

    Update: ボタンの出し分けを変更

--- src/components/Works/detail/LinkButton.tsx
+++ src/components/Works/detail/LinkButton.tsx
@@ -9,40 +9,6 @@ type PropType = {
 }
 const LinkButton: React.FC<PropType> = (props) => {
   const isExternalLink = /^https:\/\//.test(props.href)
-  const ExternalButton = styled.a`
-    display: block;
-    position: relative;
-    transition: opacity .2s ease 0s;
-    background: ${({theme}) => theme.colors.orange};
-    border-radius: 5px;
-    color: ${({theme}) => theme.colors.white};
-    font-size: 1.8rem;
-    font-weight: bold;
-    text-align: center;
-    text-decoration: none;
-    width: 80%;
-    max-width: 300px;
-    margin: 30px auto 0;
-    padding: 15px 30px;
-    &::before {
-      content: "";
-      position: absolute;
-      top: 50%;
-      right: 15px;
-      transform: translateY(-50%) rotate(45deg);
-      transition: right .2s ease 0s;
-      border-top: 2px solid ${({theme}) => theme.colors.white};
-      border-right: 2px solid ${({theme}) => theme.colors.white};
-      width: 10px;
-      height: 10px;
-    }
-    &:hover {
-      opacity: .8;
-      &::before {
-        right: 10px;
-      }
-    }
-  `
   const Button = styled(AniLink)`
     display: block;
     position: relative;
@@ -80,7 +46,7 @@ const LinkButton: React.FC<PropType> = (props) => {
 
   return (
     isExternalLink ?
-      <ExternalButton href={props.href}>{props.children}</ExternalButton> :
+      <Button as="a" href={props.href}>{props.children}</Button> :
       <Button
         paintDrip
         hex={theme.colors.orange}
