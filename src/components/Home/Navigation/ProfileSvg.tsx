ad7a308bca404b63fa9744990af9b35e2d5e381b
harutete <ai.takeuchi.10@gmail.com>
2020-11-26 22:46:37 +0900

    Remove: 不要な記述を削除

--- src/components/Home/Navigation/ProfileSvg.tsx
+++ src/components/Home/Navigation/ProfileSvg.tsx
@@ -13,9 +13,6 @@ const ProfileSvg: React.FC<Props> = (props) => (
     width={props.width}
     height={props.height}
   >
-    <svg width="794" height="817" viewBox="0 0 794 817" fill="none" xmlns="http://www.w3.org/2000/svg">
-</svg>
-
     <path
       d="M614.82 608.425C543.61 766.548 439.038 773.285 280.916 702.076C122.793 630.867 40.1004 487.623 111.31 329.5C182.519 171.377 294.31 107 409.226 129.79C584.5 189 614.82 82 725.31 292.5C765.329 375.626 660.154 507.758 614.82 608.425Z"
       fill={theme.colors.purple}
